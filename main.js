9c01dce181872103683cb208d7f1baab46f78f16
Ricardo Ramirez <ricardo.ramirez2@utdallas.edu>
2019-11-14 17:42:02 -0600

    changes

--- main.js
+++ main.js
@@ -1,70 +1,34 @@
-var fs = require('fs'),
-    fabric = require('fabric').fabric,
-    out = fs.createWriteStream(__dirname + '/helloworld.png');
+import { obj, obj2 } from "./Components/parser";
+import { ShapeMapper } from "./Components/ShapeMapper";
+import { DiagramMaker } from "./Components/DiagramMaker";
+import { Canvas } from "./Components/Canvas";
+import { ImageMaker } from "./Components/ImageMaker";
+import { ShapeMapper2 } from "./Components/ShapeMapper2";
+import { DiagramMaker2 } from "./Components/DiagramMaker2";
+import { colorMap } from "./Components/colorMap";
+import {ColorMapper} from "./Components/ColorMapper"
+import { Legend } from "./Components/Legends";
 
-var canvas = new fabric.StaticCanvas(null, {
-  backgroundColor:'white', width: 550, height: 500 });
+function main() {
+  const shapeMapper = new ShapeMapper2(obj2, new ColorMapper());
+  const collection = shapeMapper.ShapeCollection;
+  
+  const diagramMaker = new DiagramMaker2(collection);
+  
+  const totalX = diagramMaker.TotalX,
+        offsetX = diagramMaker.OffsetX,
+        offsetY = diagramMaker.OffsetY;
+  
+  var totalY = diagramMaker.TotalY;
 
-var text = new fabric.Text('Hello world', {
-  left: 100,
-  top: 100,
-  fill: '#f55',
-  angle: 15,
-});
 
-var coords = [210, 90, 350, 90];
+  const legends = new Legend(totalY, offsetX, shapeMapper.mappedcolors);
 
-var line = new fabric.Line(coords, {
-    stroke: 'black',
-    strokeWidth: 4
-});
+        totalY = legends.TotalY;
 
-var rect1 = new fabric.Rect({
-    width: 160,
-    height: 80,
-    fill: '#ff0004',
-    stroke: 'black',
-    strokeWidth: 2,
-    originX: 'center',
-    originY: 'center'
-});
+  const canvas = new Canvas(diagramMaker.Shapes,  totalX + offsetX, totalY + offsetY, legends);
+  
+  const imageMaker = new ImageMaker(canvas.canvas);
+}
 
-var rect2 = new fabric.Rect({
-    width: 160,
-    height: 80,
-    fill: '#ff0004',
-    stroke: 'black',
-    strokeWidth: 2,
-    originX: 'center',
-    originY: 'center'
-});
-
-var text1 = new fabric.Text('sample text 1', {
-  fontSize: 24,
-  originX: 'center',
-  originY: 'center'
-});
-
-var text2 = new fabric.Text('sample text 2', {
-  fontSize: 24,
-  originX: 'center',
-  originY: 'center'
-});
-
-var group1 = new fabric.Group([rect1, text1], {
-  left: 50,
-  top: 50
-});
-
-var group2 = new fabric.Group([rect2, text2], {
-  left: line.get('x2'),
-  top: 50
-});
-
-canvas.add(line, group1, group2);
-canvas.renderAll();
-
-var stream = canvas.createPNGStream();
-stream.on('data', function(chunk) {
-  out.write(chunk);
-});
+main();
